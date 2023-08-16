c2160387f56660f1f2ed3db8793169350620110a
Eluem <eulmmm@gmail.com>
2023-08-16 14:23:23 -0400

    Updated the PID controller to more correctly implement the algorithm.

--- Assets/PIDController.cs
+++ Assets/PIDController.cs
@@ -2,44 +2,32 @@ using UnityEngine;
 
 public class PIDController : MonoBehaviour
 {
-    public float kp = 1.0f;  // Proportional gain
-    public float ki = 0.0f;  // Integral gain
-    public float kd = 0.0f;  // Derivative gain
+    public Vector3 targetPos;
+    public Vector3 targetVel;
 
-    // For demonstration purposes, you can initialize Pdes and Vdes to some desired values
-    // but in a real application, you might set these values based on user input or other game logic.
-    public Vector3 Pdes = Vector3.zero;
-    public Vector3 Vdes = Vector3.zero;
+    public float frequency = 1;
+    public float damping = 1;
 
-    private Vector3 integralError = Vector3.zero;  // Integral error accumulator
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void FixedUpdate()
     {
+        float kp = (6f * frequency) * (6f * frequency) * 0.25f;
+        float kd = 4.5f * frequency * damping;
+
         float dt = Time.fixedDeltaTime;
-
-        // Calculate the g coefficient for stability
         float g = 1 / (1 + kd * dt + kp * dt * dt);
-
-        // Calculate the modified gains
         float ksg = kp * g;
-        float kig = ki * g * dt;  // Added integral component
         float kdg = (kd + kp * dt) * g;
-
-        // Current position and velocity
         Vector3 Pt0 = transform.position;
-        Vector3 Vt0 = GetComponent<Rigidbody>().velocity;  // Fixed the rigidbody call
-
-        // Calculate position and velocity errors
-        Vector3 positionError = Pdes - Pt0;
-        Vector3 velocityError = Vdes - Vt0;
-
-        // Update integral error
-        integralError += positionError * dt;
-
-        // Calculate force using PID equation
-        Vector3 F = positionError * ksg + integralError * kig + velocityError * kdg;
-
-        // Apply the force
-        GetComponent<Rigidbody>().AddForce(F);
+        Vector3 Vt0 = rb.velocity;
+        Vector3 F = (targetPos - Pt0) * ksg + (targetVel - Vt0) * kdg;
+        
+        rb.AddForce(F);
     }
 }
